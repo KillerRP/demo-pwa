0c76bc7c6cc707e9e4df8e2cde2a5760723caa99
KillerRP <rishi.patel@globalvoxinc.com>
2023-07-01 22:41:04 +0530

    - Added new code in service worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -20,3 +20,33 @@ this.addEventListener("fetch", (event) => {
     })
   );
 });
+
+// When the service worker is activated, remove old caches
+this.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event: intercept requests and serve from cache if available
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // If the request is already in the cache, serve it from there
+      if (response) {
+        return response;
+      }
+
+      // Otherwise, fetch the request from the network
+      return fetch(event.request);
+    })
+  );
+});
